13bbbe58baee7eb5884a33b8819210202870c534
prachothansingarapu <prachothan111@gmail.com>
2025-02-16 18:16:33 +0530

    commit it

--- src/componets/NewsItem.js
+++ src/componets/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsIteam extends Component {
           <div className="card" >
             <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:"1" , left: "90%"}}>{source} 
             </span>
-            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2025/01/05/1600x900/Jeff_Bezos_with_girlfriend_1684594884270_1736050109054.jpg":imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl?"https://cdn.mos.cms.futurecdn.net/Ndg2z6YZD9YyNZAi6z57R4-1200-80.gif":imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">             
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{discription}</p>
